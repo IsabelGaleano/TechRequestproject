5ac74acbb487e5f6850d4fe9e4b31d93f11b6384
Isabel <isagaleano9@gmail.com>
2023-11-19 00:50:40 -0600

    Add  sign out

--- pages/pages/profile.js
+++ pages/pages/profile.js
@@ -1,8 +1,12 @@
 // import node module libraries
 import { Col, Row, Container } from 'react-bootstrap';
-
+import { Component, useEffect, useState } from "react";
 // import widget as custom components
 import { PageHeading } from 'widgets'
+
+import { useAuthContext } from "../../context/AuthContext";
+import { useRouter } from "next/navigation";
+import DefaultDashboardLayout from "../../layouts/DefaultDashboardLayout";
 
 // import sub components
 import {
@@ -15,40 +19,60 @@ import {
 } from 'sub-components'
 
 const Profile = () => {
+
+  const { user } = useAuthContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user == null) router.push("/");
+  }, [user]);
+  
+  const Layout =
+    Component.Layout ||
+    (router.pathname && router.pathname.includes("dashboard")
+      ? router.pathname.includes("instructor") ||
+        router.pathname.includes("student")
+        ? DefaultDashboardLayout
+        : DefaultDashboardLayout
+      : DefaultDashboardLayout);
+
   return (
-    <Container fluid className="p-6">
-      {/* Page Heading */}
-      <PageHeading heading="Overview"/>
+    <Layout>
+      <Container fluid className="p-6">
+        {/* Page Heading */}
+        <PageHeading heading="Overview" />
 
-      {/* Profile Header  */}
-      <ProfileHeader />
+        {/* Profile Header  */}
+        <ProfileHeader />
 
-      {/* content */}
-      <div className="py-6">
-        <Row>
+        {/* content */}
+        <div className="py-6">
+          <Row>
 
-          {/* About Me */}
-          <AboutMe />
+            {/* About Me */}
+            <AboutMe />
 
-          {/* Projects Contributions */}
-          <ProjectsContributions />
+            {/* Projects Contributions */}
+            <ProjectsContributions />
 
-          {/* Recent From Blog */}
-          <RecentFromBlog />
+            {/* Recent From Blog */}
+            <RecentFromBlog />
 
-          <Col xl={6} lg={12} md={12} xs={12} className="mb-6">
+            <Col xl={6} lg={12} md={12} xs={12} className="mb-6">
 
-            {/* My Team */}
-            <MyTeam />
+              {/* My Team */}
+              <MyTeam />
 
-            {/* Activity Feed */}
-            <ActivityFeed />
+              {/* Activity Feed */}
+              <ActivityFeed />
 
-          </Col>
-        </Row>
-      </div>
+            </Col>
+          </Row>
+        </div>
 
-    </Container>
+      </Container>
+    </Layout>
+
   )
 }
 
